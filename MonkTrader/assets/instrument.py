656796c56001777823c33c726039179c1792b0d9
will <zsluedem06@gmail.com>
2019-01-07 22:23:58 +0800

    maint margin not main margin

--- MonkTrader/assets/instrument.py
+++ MonkTrader/assets/instrument.py
@@ -79,7 +79,7 @@ class Instrument():
 class FutureInstrument(Instrument):
     root_symbol: Optional[str] = None
     init_margin: float = 0
-    main_margin: float = 0
+    maint_margin: float = 0
 
     settlement_fee: float = 0
     settle_currency: Optional[str] = None

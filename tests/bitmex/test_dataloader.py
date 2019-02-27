c1e741f4a91ca16b0ef460ef2eb5fb5b130640fc
will <zsluedem06@gmail.com>
2019-02-27 16:02:15 +0800

    make test dataloader pass

--- tests/bitmex/test_dataloader.py
+++ tests/bitmex/test_dataloader.py
@@ -134,20 +134,20 @@ def test_bitmex_dataloader_instruments(exchange: MagicMock) -> None:
     assert TRXH19.deleverage
 
 
-@pytest.mark.xfail
 def test_bitmex_dataloader_kline_data(exchange: MagicMock) -> None:
     context = MagicMock()
-    dataloader = BitmexDataloader(exchange, context, get_resource_path())
+    with patch("MonkTrader.exchange.bitmex.data.loader.KLINE_FILE_NAME", 'test_table.hdf'):
+        dataloader = BitmexDataloader(exchange, context, get_resource_path())
 
-    instrument = MagicMock()
-    instrument.symbol = "XBTZ15"
-    context.now = utc_datetime(2015, 12, 25, 11, 40)
+        instrument = MagicMock()
+        instrument.symbol = "XBTZ15"
+        context.now = utc_datetime(2015, 12, 25, 11, 40)
 
-    assert dataloader.get_last_price(instrument) == 454.00
-    kline_df = dataloader.get_kline(instrument, 50)
-    assert len(kline_df) == 50
-    assert kline_df.index[-1] == utc_datetime(2015, 12, 25, 11, 39)
+        assert dataloader.get_last_price(instrument) == 454.00
+        kline_df = dataloader.get_kline(instrument, 50)
+        assert len(kline_df) == 50
+        assert kline_df.index[-1] == utc_datetime(2015, 12, 25, 11, 39)
 
-    context.now = utc_datetime(2016, 1, 1, 11, 12)
+        context.now = utc_datetime(2016, 1, 1, 11, 12)
 
-    assert dataloader.get_last_price(instrument) == 0
+        assert dataloader.get_last_price(instrument) == 0

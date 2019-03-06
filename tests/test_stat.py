3d3f74ba26c786518a85f48336938cefd5a591f3
will <zsluedem06@gmail.com>
2019-03-06 14:43:32 +0800

    add stat

---
+++ tests/test_stat.py
@@ -0,0 +1,14 @@
+from MonkTrader.stat import Statistic
+from unittest.mock import MagicMock
+from MonkTrader.utils.timefunc import utc_datetime
+
+def test_statistic():
+    account = MagicMock()
+    context = MagicMock()
+    context.now = utc_datetime(2018,1,1)
+    account.wallet_balance = 1000000
+    stat = Statistic(account, context)
+
+    stat.collect_daily()
+
+    assert stat.collected_data == [(utc_datetime(2018,1,1), 1000000)]
